e1992df41b58bf392474aeb3d47c5842d20faec7
Excellence Oyeniran <oyeniranexcellence>
2021-05-27 12:04:11 +0100

    make a pricing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { TryOuts } from './pages/biometrics'
+import { Pricing } from './pages/pricing-pages'
 
 export const AppRoutes = () => {
   return (
     <Router>
       <Route path="/biometrics" component={TryOuts} />
+      <Route path="/pricing" component={Pricing} />
     </Router>
   )
 }
---
+++ src/pages/pricing-pages/index.js
@@ -0,0 +1,141 @@
+import React from 'react'
+import './style.scss'
+
+export const Pricing = () => {
+  return (
+    <div className="pricing-page-one">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 col-12">
+            <p className="logo-print">
+              <span>s</span>hop <span>u</span>s
+            </p>
+          </div>
+          <div className="col-md-6 col-12">
+            <ul>
+              <li>about</li>
+              <li>products</li>
+              <li>shops</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="center">
+            <h2 className="topic">
+              Flexible <span className="black">Plans</span>
+            </h2>
+            <h4 className="sub-topic">
+              Choose a plan that works best for you and your team
+            </h4>
+          </div>
+        </div>
+
+        <div className="row plans">
+          <div className="plan">
+            <div className="header">
+              <p className="starter">
+                <i class="bi bi-dice-1"></i>
+              </p>
+              <p>
+                <h3>Tod Pack</h3>
+                <h5>1 Month / 3000 Euros</h5>
+              </p>
+            </div>
+            <div className="body">
+              <p>
+                <i class="bi bi-check-circle-fill"></i>
+                <span> Free Messaging</span>
+              </p>
+              <p>
+                <i class="bi bi-check-circle-fill"></i>
+                <span> Free Calls</span>
+              </p>
+              <p>
+                <i class="bi bi-x-circle-fill"></i>
+                <span> Internet Access </span>
+              </p>
+              <p>
+                <i class="bi bi-x-circle-fill"></i>
+                <span> Cloud Storage</span>
+              </p>
+              <p>
+                <button className="btn btn-starter">
+                  Choose Plan <i class="bi bi-arrow-right-short"></i>
+                </button>
+              </p>
+            </div>
+          </div>
+          <div className="plan">
+            <div className="header">
+              <p className="big-boy">
+                <i class="bi bi-dice-2"></i>
+              </p>
+              <p>
+                <h3>Teen Plan</h3>
+                <h5>1 Month / 6500 Euros</h5>
+              </p>
+            </div>
+            <div className="body">
+              <p>
+                <i class="bi bi-check-circle-fill"></i>
+                <span> Free Messaging</span>
+              </p>
+              <p>
+                <i class="bi bi-check-circle-fill"></i>
+                <span> Free Calls</span>
+              </p>
+              <p>
+                <i class="bi bi-check-circle-fill"></i>
+                <span> Internet Access </span>
+              </p>
+              <p>
+                <i class="bi bi-x-circle-fill"></i>
+                <span> Cloud Storage</span>
+              </p>
+              <p>
+                <button className="btn btn-teen">
+                  Choose Plan <i class="bi bi-arrow-right-short"></i>
+                </button>
+              </p>
+            </div>
+          </div>
+          <div className="plan">
+            <div className="header">
+              <p className="adult">
+                <i class="bi bi-dice-3"></i>
+              </p>
+              <p>
+                <h3>Adult Pack</h3>
+                <h5>1 Month / 3000 Euros</h5>
+              </p>
+            </div>
+            <div className="body">
+              <p>
+                <i class="bi bi-check-circle-fill"></i>
+                <span> Free Messaging</span>
+              </p>
+              <p>
+                <i class="bi bi-check-circle-fill"></i>
+                <span> Free Calls</span>
+              </p>
+              <p>
+                <i class="bi bi-check-circle-fill"></i>
+                <span> Internet Access </span>
+              </p>
+              <p>
+                <i class="bi bi-check-circle-fill"></i>
+                <span> Cloud Storage</span>
+              </p>
+              <p>
+                <button className="btn btn-adult">
+                  Choose Plan <i class="bi bi-arrow-right-short"></i>
+                </button>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
